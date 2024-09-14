3e623fcf9fa5d499837152ad5a8782e293e67f1e
Richard <69055743+Richard-Byers@users.noreply.github.com>
2024-09-14 21:25:36 +0100

    Brought across local files, login and registration system implemented thus far

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+class UserController extends Controller
+{
+    public function register(Request $request)
+    {
+        $incomingFields = $request->validate([
+            'username' => ['required', 'min:3', 'max:20', Rule::unique('users', 'username')],
+            'email' => ['required', 'email', 'max:254', Rule::unique('users', 'email')],
+            'password' => ['required', 'min:8', 'confirmed']
+        ]);
+        $user = User::create($incomingFields);
+        auth()->login($user);
+        return redirect('/')->with('success', 'You have successfully registered an account.');
+    }
+
+    public function login(Request $request)
+    {
+        $incomingFields = $request->validate([
+            'loginusername' => 'required',
+            'loginpassword' => 'required'
+        ]);
+
+        if (auth()->attempt(['username' => $incomingFields['loginusername'], 'password' => $incomingFields['loginpassword']])) {
+            $request->session()->regenerate();
+            return redirect('/')->with('success', 'You have successfully logged in.');
+        } else {
+            return redirect('/')->with('failure', "Wrong username and/or password entered.");
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        auth()->logout();
+        return redirect('/')->with('success', 'You have successfully logged out.');
+    }
+
+    public function showCorrectHomePage()
+    {
+        if (auth()->check()) {
+            return view('homepage-feed');
+        } else {
+            return view('homepage');
+        }
+    }
+}
