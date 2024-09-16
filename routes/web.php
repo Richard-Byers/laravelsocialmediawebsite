88da89155da314ea3ab060f5916bb9d8a0855faa
Richard <69055743+Richard-Byers@users.noreply.github.com>
2024-09-16 22:32:28 +0100

    Basic create a post functionality implemented

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 
+// User Related Routes
 Route::get('/', [UserController::class, "showCorrectHomePage"]);
-
 Route::post('/register', [UserController::class, 'register']);
 Route::post('/login', [UserController::class, 'login']);
 Route::post('/logout', [UserController::class, 'logout']);
+
+//Blog Related Routes
+Route::get('/create-post', [PostController::class, 'showCreateForm']);
+Route::post('/create-post', [PostController::class, 'storeNewPost']);
+Route::get('/post/{post}', [PostController::class, 'viewSinglePost']);
